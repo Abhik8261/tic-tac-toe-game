f371080a43f6abb5900ef55ca49e9f5512977547
Abhik8261 <abhik.tech8261@gmail.com>
2022-06-09 08:14:55 +0530

    X&0 can be show by clicking

--- script.js
+++ script.js
@@ -18,16 +18,18 @@ const checkwin=()=>{
 
 
 //game logic
-let boxes =document.getElementsByClassName("box");
-Array.from(boxes).forEach(element=>{
-    let boxtext=document.querySelector('.boxtext')
-    element.addEventListener('click',(e)=>{
-        if(boxes.innerText===''){
-            boxes.innerText=turn;
-            changeTurn();
-            audioturn.play();
-            checkwin();
-            document.getElementsByClassName(turn)[0].innerText="Turn for"+turn;
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', ()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
+            checkWin();
+            if (!isgameover){
+                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+            } 
         }
     })
 })
